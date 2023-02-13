3465ce60a8e98e59f409370df09adb3c52d8e433
Yannick Cadin <yannick@diablotin.fr>
2023-02-13 23:19:25 +0100

    Finition de export-OVA.sh

--- export-OVA.sh
+++ export-OVA.sh
@@ -1,4 +1,4 @@
-if [[ "${1,,}" == *-h* ]]
+if [[ "${1,,}" == -h || "${1,,}" == --help ]]
 then
 	echo "Usage: $(basename $0) [ NOM_ARCHIVE ]"
 	exit 0
@@ -7,7 +7,25 @@ fi
 NOM_ARCHIVE=${1:-XW335-MicroK8s}
 DOSSIER_OVA="$HOME/Documents/"
 
+if [ -s "$DOSSIER_OVA$NOM_ARCHIVE".ova ]
+then
+	read -p "Une archive '$DOSSIER_OVA$NOM_ARCHIVE.ova' existe déjà, continuer [N/o] ? " CONTINUER
+
+	if [[ "${CONTINUER^}" != O* ]]
+	then
+		echo Abandon
+		exit 1
+	else
+		rm "$DOSSIER_OVA$NOM_ARCHIVE".ova "$DOSSIER_OVA$NOM_ARCHIVE".SHA256SUM.txt 2>/dev/null
+	fi
+fi
+
 read -p "Copier l'OVA sur diablotin.fr [O/n] ? " COPIER
+
+if [[ "${COPIER^}" != N* ]]
+then
+	read -sp "Mot de passe SSH (vide si authentification par clef) : " PASSE_SSH
+fi
 
 vagrant halt	# à la place de vagrant ssh $NOEUD -c "sudo halt -p" appliqué à chaque machine
 
@@ -17,5 +35,11 @@ if [[ "${COPIER^}" != N* ]]
 then
 	cd "$DOSSIER_OVA"
 	sha256sum "$NOM_ARCHIVE".ova | tee $(basename "$NOM_ARCHIVE".SHA256SUM.txt)
-	scp "$NOM_ARCHIVE"* wwwbis:IB/XW335/ && echo OVA copiée
+
+	if [ -z "$PASSE_SSH" ]
+	then
+		scp "$NOM_ARCHIVE"* wwwbis:IB/XW335/ && echo OVA copiée
+	else
+		sshpass -p $PASSE_SSH scp "$NOM_ARCHIVE"* wwwbis:IB/XW335/ && echo "'$NOM_ARCHIVE'.ova copié"
+	fi
 fi
